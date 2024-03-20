e0bd137df6fa8ca43fb9e78f095cc315676c36fb
Subash Jirel <subashjrl@gmail.com>
2024-03-20 21:27:23 +0545

    set up mongodb and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
+require('dotenv').config();
 //middleware
 app.use(express.json());
 
@@ -11,4 +13,13 @@ app.get('/', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const port = 5000;
-app.listen(port, console.log(`App is listening on the port ${port}...`));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`App is listening on the port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
--- routes/tasks.js
+++ routes/tasks.js
@@ -9,8 +9,8 @@ const {
 } = require('../controllers/tasks');
 
 router.get('/', getAllTasks);
+router.post('/', createTask);
 router.get('/:id', getTask);
-router.post('/:id', createTask);
 router.patch('/:id', updateTask);
 router.delete('/:id', deleteTask);
 
